9ac41d33e3648e28743297999194e78a220631ad
mandeep1012 <30328595+mandeep1012@users.noreply.github.com>
2019-08-13 12:29:07 +0530

    personproject

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    { img: 22, name: "John", job: "Developer" },
+    { img: 21, name: "Bob", job: "Designer" },
+    { img: 27, name: "Peter", job: "Artist" }
+  ];
+  return (<section>
+    <Person person={people[0]} />
+    <Person person={people[1]}>
+      Childrennnnnn
+    </Person>
+    <Person person={people[2]} />
+  </section>);
+};
+
+const Person = (props) => {
+  const{img,name,job} = props.person;
+  const {children} = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <img src={url} alt="" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </div>
   );
-}
+};
 
 export default App;
